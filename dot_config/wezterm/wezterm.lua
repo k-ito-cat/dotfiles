419812087323039f09861500de3f6d065cba756c
k-ito-cat <kyohy163@gmail.com>
2026-04-04 23:35:03 +0900

    feat(wezterm): タブ表示をretroとディレクトリ名ベースに変更

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,6 +9,40 @@ local target = wezterm.target_triple
 local is_darwin = target:find("apple") ~= nil
 local is_windows = target:find("windows") ~= nil
 
+local function basename(path)
+  if not path or path == '' then
+    return nil
+  end
+
+  local normalized = path:gsub('[\\/]$', '')
+  return normalized:match('([^/\\]+)$') or normalized
+end
+
+local function tab_title(tab_info)
+  local explicit_title = tab_info.tab_title
+  if explicit_title and #explicit_title > 0 then
+    return explicit_title
+  end
+
+  local pane = tab_info.active_pane
+  local cwd = pane.current_working_dir
+
+  if cwd then
+    local dir = cwd.file_path or tostring(cwd)
+    local title = basename(dir)
+    if title and #title > 0 then
+      return title
+    end
+  end
+
+  local process_name = basename(pane.foreground_process_name)
+  if process_name and #process_name > 0 then
+    return process_name
+  end
+
+  return pane.title
+end
+
 if is_windows then
   config.default_domain = 'WSL:Ubuntu'
   config.allow_win32_input_mode = true
@@ -70,6 +104,16 @@ else
 end
 config.default_cursor_style = "SteadyBar"
 config.window_padding = { left = 10, right = 10, top = 8, bottom = 8 }
+config.use_fancy_tab_bar = false
+config.show_tab_index_in_tab_bar = false
+config.tab_bar_style = {
+  new_tab = wezterm.format {
+    { Text = ' + ' },
+  },
+  new_tab_hover = wezterm.format {
+    { Text = ' + ' },
+  },
+}
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = 'NeverPrompt'
@@ -78,8 +122,8 @@ config.window_close_confirmation = 'NeverPrompt'
 if is_windows then
   config.win32_system_backdrop = 'Disable'
 end
-config.window_background_opacity = 0.85
-config.text_background_opacity = 0.9
+config.window_background_opacity = 1.0
+config.text_background_opacity = 1.0
 
 config.keys = {
   { key = 'c', mods = 'CTRL|SHIFT', action = act.CopyTo 'Clipboard' },
@@ -113,5 +157,10 @@ config.keys = {
 
   { key = 'r', mods = 'CTRL|SHIFT', action = act.ReloadConfiguration },
 }
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, pane_config, hover, max_width)
+  local title = wezterm.truncate_right(tab_title(tab), math.max(max_width - 2, 0))
+  return { { Text = ' ' .. title .. ' ' } }
+end)
 
 return config
