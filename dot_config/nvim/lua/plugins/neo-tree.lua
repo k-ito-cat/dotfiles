8bb711603de8bd385065f75b20a9de06f44f2163
k-ito-cat <kyohy163@gmail.com>
2026-04-12 10:25:41 +0900

    feat(nvim): 起動時にneo-treeを表示

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -8,10 +8,23 @@ return {
       "MunifTanjim/nui.nvim",
     },
     cmd = "Neotree",
+    event = "VimEnter",
+    init = function()
+      vim.api.nvim_create_autocmd("VimEnter", {
+        once = true,
+        callback = function()
+          vim.cmd("Neotree show reveal")
+        end,
+      })
+    end,
     opts = {
       close_if_last_window = true,
       popup_border_style = "rounded",
       filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_hidden = false,
+        },
         follow_current_file = { enabled = true },
         hijack_netrw_behavior = "open_default",
       },
